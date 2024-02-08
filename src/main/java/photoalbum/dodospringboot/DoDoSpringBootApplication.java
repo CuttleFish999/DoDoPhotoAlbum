b028dbd14bc5a3a565cab5dd514dcebc0b47e071
CuttleFish <b9804320975@gmail.com>
2024-02-08 23:47:26 +0800

    init

---
+++ src/main/java/photoalbum/dodospringboot/DoDoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package photoalbum.dodospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoDoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoDoSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/photoalbum/dodospringboot/DoDoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package photoalbum.dodospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoDoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
