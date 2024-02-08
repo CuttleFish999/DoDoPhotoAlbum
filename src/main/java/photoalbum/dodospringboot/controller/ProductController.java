f29b2fbe4c9620ee75da4b2aa755bfc7ca0bbda0
CuttleFish <b9804320975@gmail.com>
2024-02-09 00:46:42 +0800

    ADD Product Controller - ProductController

---
+++ src/main/java/photoalbum/dodospringboot/controller/ProductController.java
@@ -0,0 +1,29 @@
+package photoalbum.dodospringboot.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import photoalbum.dodospringboot.model.Product;
+import photoalbum.dodospringboot.service.ProductService;
+
+@Repository
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<Product> getProduct(@PathVariable Integer productId){
+        Product product = productService.getProductById(productId);
+
+        if(product != null){
+            return ResponseEntity.status(HttpStatus.OK).body(product);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+}
